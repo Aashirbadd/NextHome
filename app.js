a5c0b3ef406dfebad197500c65dcfc34b8bbe267
Mikail Munir <mikailmunir01@gmail.com>
2021-11-25 18:38:18 -0700

    create user

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(morgan("combined"));
+app.use(morgan("short"));
 app.use(express.static("./src"));
 
 function getConnection(){
@@ -28,6 +28,26 @@ app.get("/listings", (req,res) => {
     res.json([listings1,listings2,listings3]);
 });
 
+app.post("/create_user", (req, res) => {
+    const firstname = req.body.create_first_name;
+    const lastname = req.body.create_last_name;
+    const email = req.body.create_email;
+    const password = req.body.create_password;
+
+    const queryString = "INSERT INTO User (FName, LName, Email, Password) VALUES (?,?,?,?);"
+
+    getConnection().query(queryString, [firstname, lastname, email, password], (err, results, fields) =>{ 
+        if(err){
+            console.log("Failed to insert new user: " + err)
+            res.sendStatus(500)
+            return
+        }
+        
+        console.log("Inserted a new user with id: ", results.userID)
+        res.end()
+    })
+})
+
 app.post("/post_listing", (req,res) => {
 
     const mlsCode = req.body.create_MLS_code;
@@ -38,24 +58,27 @@ app.post("/post_listing", (req,res) => {
     const sqft = req.body.create_square;
     const bedrooms = req.body.create_bedrooms;
     const bathrooms = req.body.create_bathrooms;
+    const areaName = req.body.create_area_name;
+    const brokerage = req.body.create_brokerage_website;
+    const realtor = req.body.create_realtor_website;
+    const user = req.body.create_user_id;
 
 
     const queryString = "INSERT INTO Listings (`MLS Code`, `Basement Type`, `Description`, Price, SquareFootage, Bedrooms, Bathrooms, Address, AreaName, BrokerageWebsite, RealtorWebsite, UserID) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);"
-    getConnection().query(queryString, [mlsCode, basementType, description, price, sqft, bedrooms, bathrooms, address, 'NE', 'realtor.ca','realtor.ca', 007], (err, results, fields)=>{
+    getConnection().query(queryString, [mlsCode, basementType, description, price, sqft, bedrooms, bathrooms, address, areaName, brokerage, realtor, user], (err, results, fields)=>{
         if(err){
             console.log("Failed to insert new listing: " + err)
             res.sendStatus(500)
             return
         }
         
-        console.log("Inserted a new listing with id: ", results.isListings)
+        console.log("Inserted a new listing with id: ", results.idListings)
         res.end()
     })
 
     res.end()
 })
 
-//localhost:3000
 app.listen(3000, () => {
     console.log("Server is up and listening on 3000....")
 });
