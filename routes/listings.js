e51806d80760c0b054ff366033a2ea6c7d6f8069
Mikail Munir <mikailmunir01@gmail.com>
2021-12-13 19:30:09 -0700

    desending listings fixed

--- routes/listings.js
+++ routes/listings.js
@@ -12,7 +12,7 @@ function getConnection(){
 }
 
 router2.get("/get_listings/", (req, res, next) => {
-    const queryString = "SELECT * FROM Listings;";
+    const queryString = "SELECT * FROM Listings ORDER BY idListings DESC ;";
     getConnection().query(queryString, (err, rows, fields) => {
         if(err){
             console.log("Failed to query for listings: " + err)
@@ -26,7 +26,7 @@ router2.get("/get_listings/", (req, res, next) => {
 
 router2.get("/get_listings/:id", (req, res, next) => {
     const listingID = req.params.id;
-    const queryString = "SELECT * FROM Listings, Brokerage, Realtors ORDER BY idListings DESC WHERE idListings = ? AND (BrokerageWebsite = Brokerage.Website) AND (RealtorWebsite = Realtors.Website);";
+    const queryString = "SELECT * FROM Listings, Brokerage, Realtors WHERE idListings = ? AND (BrokerageWebsite = Brokerage.Website) AND (RealtorWebsite = Realtors.Website);";
     const queryInserts = [listingID];
     getConnection().query(queryString, queryInserts, (err, rows, fields) => {
         if(err){
